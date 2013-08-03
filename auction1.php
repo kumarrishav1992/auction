d8e48517ccd122e5b1949ebc108661658ddb3cd4
kumarrishav <kumarrishav1992@gmail.com>
2013-08-03 23:39:25 +0100

    auction

--- auction1.php
+++ auction1.php
@@ -1,5 +1,30 @@
 <html>
-<body>
+<head>
+<style>
+#head{
 
+height: 285px; 
+width:  175px; 
+border: 3px groove green; 
+border-radius:5px;
+background-color: yellow; 
+}
+</style>
+</head>
+<body align='center' bgcolor='pink'>
+<h1>PLAYER BIDDINDG 2013</h1>
+<div id='head'>
+<table border='1'>
+<tr><th>SUMMARY</th></tr>
+<tr><th><h3>PLAYERS DETAIL<h3></th></tr>
+<tr><td><a href='bat.php'>BATSMAN</a></th></tr>
+<tr><th>BIDDING ZONE</th></tr>
+<tr><td><a href='bid.php'>BIDDING</a></th></tr>
+<tr><td><a href='ADMIN1.php'>ADMIN</a></th></tr>
+<tr><th><a href='result.php'>BIDDING RESULT</a></th></tr>
+<tr><th><a href='rules.php'>RULES</a></th></tr>
+<tr><th><a href='contact.php'>CONTACTS</a></th></tr>
+</table>
+</div>
 </body>
 </html>
